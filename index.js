924aa81a318676b8fda93ae098044f15ac4bd7f5
amananurag <amananurag.20@gmail.com>
2025-07-21 15:14:33 +0530

    api call async await

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-fetch("https://dog.ceo/api/breeds/image/random").then((data)=>{
-    console.log("Data",data);
-    return data.json()
-}).then((data)=>{
+console.log("hiihi")
+
+async function fetchData(){
+    
+   console.log("start")
+
+    const response= await fetch("https://dog.ceo/api/breeds/image/random");//1 sec
+   
+    const data = await response.json();
     console.log(data)
-})
+
+    console.log("end")
+
+}
+fetchData()
+
+console.log("byee")
+
 
 
 
